69e30cc05675ba64f18d0048cbd18b4a0109c55d
平井 <hiraitakahiro0923@gmail.com>
2024-12-09 01:44:32 +0900

    feat:shadncn ui追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -48,6 +49,9 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <Button>
+          shadncn
+        </Button>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
