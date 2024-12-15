f86d731ac6fa7e770d67a515a708387b715cb2c5
平井 <hiraitakahiro0923@gmail.com>
2024-12-15 22:21:02 +0900

    fix:コンポーネント化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,9 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
 import { AppSidebar } from '@/components/app-sidebar';
+import Header from '@/components/Header';
 import { ThemeProvider } from '@/components/theme-provider';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -50,36 +42,9 @@ export default function RootLayout({
         >
           <SidebarProvider>
             <AppSidebar />
-            {/* <main>
-              <SidebarTrigger />
-              {children}
-            </main> */}
             <SidebarInset>
-              <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
-                <div className='flex items-center gap-2 px-4'>
-                  <SidebarTrigger className='-ml-1' />
-                  <Separator orientation='vertical' className='mr-2 h-4' />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className='hidden md:block'>
-                        <BreadcrumbLink href='#'>Building Your Application</BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className='hidden md:block' />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
-                </div>
-              </header>
-              <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-                <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-                  <div className='aspect-video rounded-xl bg-muted/50' />
-                  <div className='aspect-video rounded-xl bg-muted/50' />
-                  <div className='aspect-video rounded-xl bg-muted/50' />
-                </div>
-                <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
-              </div>
+              <Header />
+              {children}
             </SidebarInset>
           </SidebarProvider>
         </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Browser } from '@/components/Chart/browser';
 import { View } from '@/components/Chart/view';
 
-import styles from './page.module.scss';
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div className={styles.cards}>
-        <View />
-        <Browser />
+    <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+      <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
+        <div className='aspect-video rounded-xl bg-muted/50'>
+          <View />
+        </div>
+        <div className='aspect-video rounded-xl bg-muted/50'>
+          <Browser />
+        </div>
+        <div className='aspect-video rounded-xl bg-muted/50' />
       </div>
+      <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
     </div>
   );
 }
