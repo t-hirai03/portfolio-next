f39cb1877d144df3b84bc1ea07a0cdc09e8616cd
平井 <hiraitakahiro0923@gmail.com>
2024-12-15 23:07:36 +0900

    feat:テーブル追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,95 @@
 import { Browser } from '@/components/Chart/browser';
 import { View } from '@/components/Chart/view';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+// import styles from './page.module.scss';
 
 export default function Home() {
+  const invoices = [
+    {
+      invoice: 'INV001',
+      paymentStatus: 'Paid',
+      totalAmount: '$250.00',
+      paymentMethod: 'Credit Card',
+    },
+    {
+      invoice: 'INV002',
+      paymentStatus: 'Pending',
+      totalAmount: '$150.00',
+      paymentMethod: 'PayPal',
+    },
+    {
+      invoice: 'INV003',
+      paymentStatus: 'Unpaid',
+      totalAmount: '$350.00',
+      paymentMethod: 'Bank Transfer',
+    },
+    {
+      invoice: 'INV004',
+      paymentStatus: 'Paid',
+      totalAmount: '$450.00',
+      paymentMethod: 'Credit Card',
+    },
+    {
+      invoice: 'INV005',
+      paymentStatus: 'Paid',
+      totalAmount: '$550.00',
+      paymentMethod: 'PayPal',
+    },
+    {
+      invoice: 'INV006',
+      paymentStatus: 'Pending',
+      totalAmount: '$200.00',
+      paymentMethod: 'Bank Transfer',
+    },
+    {
+      invoice: 'INV007',
+      paymentStatus: 'Unpaid',
+      totalAmount: '$300.00',
+      paymentMethod: 'Credit Card',
+    },
+  ];
+
   return (
     <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
       <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-        <div className='aspect-video rounded-xl bg-muted/50'>
+        <div className='rounded-xl bg-muted/50'>
           <View />
         </div>
-        <div className='aspect-video rounded-xl bg-muted/50'>
+        <div className='rounded-xl bg-muted/50'>
           <Browser />
         </div>
         <div className='aspect-video rounded-xl bg-muted/50' />
       </div>
-      <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' />
+      <div className='min-h-[100vh] flex-1 rounded-xl md:min-h-0'>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[100px]'>Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className='text-right'>Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className='font-medium'>{invoice.invoice}</TableCell>
+                <TableCell>{invoice.paymentStatus}</TableCell>
+                <TableCell>{invoice.paymentMethod}</TableCell>
+                <TableCell className='text-right'>{invoice.totalAmount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
