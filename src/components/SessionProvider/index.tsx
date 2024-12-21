68aa96b8adf4705b227188a1c6ef2811acdd08fc
平井 <hiraitakahiro0923@gmail.com>
2024-12-22 00:59:20 +0900

    feat:ログイン情報送信するカスタムディメンションを用意

--- src/components/SessionProvider/index.tsx
+++ src/components/SessionProvider/index.tsx
@@ -1,11 +1,34 @@
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 type Props = {
   children?: React.ReactNode;
 };
 
 export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <AuthSessionTracking>{children}</AuthSessionTracking>
+    </SessionProvider>
+  );
+};
+
+const AuthSessionTracking = ({ children }: { children: React.ReactNode }) => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session && session.user) {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'loginUserInfo',
+        userName: session.user.name,
+        userEmail: session.user.email,
+      });
+      console.log(window.dataLayer);
+    }
+  }, [session]);
+
+  return <>{children}</>;
 };
