9c66282529815bfdc8ef7ef1f6bd85e480ca07a6
平井 <hiraitakahiro0923@gmail.com>
2025-01-01 01:28:29 +0900

    feat:サイトマップ設定処理追加

---
+++ sitemap.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteUrl: 'https://portfolio-next-ts-orpin.vercel.app', // サイトURL
+  generateRobotsTxt: true, // robots.txt の生成
+  sitemapSize: 7000, // サイトマップの最大サイズ
+  outDir: './out', // 出力先ディレクトリ
+  exclude: ['/login'], // 除外するURLの指定
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/', // 全体許可
+        disallow: ['/login'], // /login のみ除外
+      },
+    ],
+  },
+};
